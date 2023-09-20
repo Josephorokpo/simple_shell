3629805e39f3bd51b520066eafe4285a725c83fc
josephorokpo <josephorokpo@gmail.com>
2023-09-20 02:21:31 -0700

    Updated

--- main.c
+++ main.c
@@ -9,53 +9,57 @@
  */
 int main(int ac, char **av)
 {
-	int main_loop = 1;
-	int exit_status = 0;
-	int errors = 0;
-	int is_executable_flag;
-	char **cmd = NULL;
+    int main_loop = 1;
+    int exit_status = 0;
+    int errors = 0;
+    int is_executable_flag;
+    char **cmd = NULL;
 
-	ac = ac;
+    ac = ac;
 
-	while (main_loop)
-	{
-		cmd = fetch_command(&main_loop);
-		if (cmd != NULL)
-		{
-			/* Check if the command is a comment (starts with '#') */
-			if (cmd[0][0] == '#' && cmd[0][1] == '\0')
-			{
-				free_command(cmd);
-				continue;
-			}
+    while (main_loop)
+    {
+        cmd = fetch_command(&main_loop);
+        if (cmd != NULL)
+        {
+            /* Check if the command is a comment (starts with '#') */
+            if (cmd[0][0] == '#' && cmd[0][1] == '\0')
+            {
+                free_command(cmd);
+                continue;
+            }
 
-			if (strcmp(cmd[0], "exit") == 0)
-			{
-    				free_command(cmd);
-   				break;
-			}
-			if (strcmp(cmd[0], "env") == 0)
-			{
-				print_environment();
-				exit_status = 0;
-				free_command(cmd);
-				continue;
-			}
-			is_executable_flag = is_executable(&cmd[0]);
-			if (is_executable_flag == 0)
-			{
-				(errors)++;
-				fprintf(stderr, "%s: %d: %s: COMMAND NOT FOUND\n", av[0], errors, cmd[0]);
-				exit_status = 127;
-				free_command(cmd);
-				continue;
-			}
-			else if (is_executable_flag == 1)
-				findExecutablePath(&cmd[0]);
+            if (strcmp(cmd[0], "exit") == 0)
+            {
+                if (cmd[1] != NULL)
+                {
+                    exit_status = atoi(cmd[1]);
+                }
+                free_command(cmd);
+                exit(exit_status);
+            }
+            if (strcmp(cmd[0], "env") == 0)
+            {
+                print_environment();
+                exit_status = 0;
+                free_command(cmd);
+                continue;
+            }
+            is_executable_flag = is_executable(&cmd[0]);
+            if (is_executable_flag == 0)
+            {
+                (errors)++;
+                fprintf(stderr, "%s: %d: %s: COMMAND NOT FOUND\n", av[0], errors, cmd[0]);
+                exit_status = 127;
+                free_command(cmd);
+                continue;
+            }
+            else if (is_executable_flag == 1)
+                findExecutablePath(&cmd[0]);
 
-			exec_command(cmd, &errors, &exit_status);
-		}
-	}
+            exec_command(cmd, &errors, &exit_status);
+        }
+    }
 
-	return (exit_status);
+    return (exit_status);
 }
